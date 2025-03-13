b66d69083cb90afb1f7979f3fc92d22121d28c6c
DSolight <mbros.ars@gmail.com>
2025-03-13 14:47:52 +0700

    fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,21 @@
 import React from "react"
 import Header from "../../components/Header/Header"
 import { ReactComponent as HomeMain } from "../../assets/homemain.svg"
+import "./Home.css"
 
 export default function Home() {
   return (
     <div>
       <Header />
       <main>
-        <div>
-          <h1>сервис по поиску публикаций о компании по его ИНН</h1>
-          <p>Комплексный анализ публикаций, получение данных в формате PDF на электронную почту.</p>
-          <button>Запросить данные</button>
+        <div className="hero">
+          <div className="hero-text">
+            <h1>сервис по поиску публикаций о компании по его ИНН</h1>
+            <p>Комплексный анализ публикаций, получение данных в формате PDF на электронную почту.</p>
+            <button>Запросить данные</button>
+          </div>
+          <HomeMain />
         </div>
-        <HomeMain />
       </main>
     </div>
   )
